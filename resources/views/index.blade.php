9ed432a4e45d61fb2914adfc01b8d3a059c6edbf
ShoContain <perblue.666@gmail.com>
2020-04-15 12:00:45 +0900

    movies-app

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+
+@section('content')
+   <div class="container mx-auto px-4 pt-16">
+       <div class="popular-movies">
+           <h2 class="">人気の映画</h2>
+       </div>
+   </div>
+@endsection
